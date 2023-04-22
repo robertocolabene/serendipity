21a5460dcc9fc42208d113d60400ce973c0aae72
Roberto Colabene <robertocolabene@gmail.com>
2023-04-22 20:43:38 +0200

    dev

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
-import { Suspense } from 'react'
+import React, { Suspense } from 'react'
 import Link from 'next/link'
 import { Metadata } from 'next'
 import * as components from '@/components'
+
+React
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,103 +19,22 @@ export async function generateMetadata(): Promise<Metadata> {
 
 export default async function Home() {
   //const [seo] = await Promise.all([getSeo()]);
-  const seo = {};
+  const blocks = [
+    components['Banner'],
+    components['Carousel']
+  ];
 
   return (
-    <>
-      <components.Banner/>
-      <components.Carousel/>
-      <Prova seo={seo}/>
+    <>  
+      {blocks.map((block,index) => 
+        <React.Fragment key={index}>
+          {block}
+        </React.Fragment>
+      )}
     </>  
   )
 }
 
 
-function Prova(props: any) {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          <Suspense fallback={<div>Loading...</div>}>
-          Get Sstarted by {props.seo.test} asdasd {props.seo.asd} editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-          </Suspense>
-        </p>
-        <Link href="/about">About</Link>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+//<components.Banner/>
+//{/* @ts-expect-error Server Component */} <components.Carousel/>
